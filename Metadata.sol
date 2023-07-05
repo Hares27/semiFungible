112ab2734a110da78348ae29a7c3afb8c75b25f9
Hares27 <107697629+Hares27@users.noreply.github.com>
2023-07-05 20:36:53 +0530

    Update Metadata.sol

--- Metadata.sol
+++ Metadata.sol
@@ -1,1 +1,20 @@
 
+// SPDX-License-Identifier: GPL-3.0
+
+
+pragma solidity ^0.8.0;
+import "./Token.sol";
+
+contract Metadata{
+     
+     Token public token;
+
+     constructor(address _tokenAddress){
+         token=Token(_tokenAddress);
+     }
+
+     uint public tokensMinted=token.numberOfTokensMinted();
+
+     
+
+}
